8441dceb15a4bc38fa2acd842b143110d75fa9f2
Max Weller <igitt@max-weller.de>
2012-11-15 19:05:03 +0100

    Added license header and LICENSE file

--- mw-android-friendica-01/src/de/wikilab/android/friendica01/FragmentParentListener.java
+++ mw-android-friendica-01/src/de/wikilab/android/friendica01/FragmentParentListener.java
@@ -1,5 +1,7 @@
 package de.wikilab.android.friendica01;
-
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 public interface FragmentParentListener {
 	
 	void OnFragmentMessage(String message, Object arg1, Object arg2);
--- mw-android-friendica-01/src/de/wikilab/android/friendica01/GenericContentActivity.java
+++ mw-android-friendica-01/src/de/wikilab/android/friendica01/GenericContentActivity.java
@@ -1,5 +1,7 @@
 package de.wikilab.android.friendica01;
-
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
