773fed1c1c409297208d17bdbec238ecffe44af6
Max Weller <git@max-weller.de>
2013-08-28 21:13:17 +0200

    cleanup

--- libpulltorefresh/gen/com/handmark/pulltorefresh/library/R.java
+++
@@ -1,7 +0,0 @@
-/*___Generated_by_IDEA___*/
-
-package com.handmark.pulltorefresh.library;
-
-/* This stub is for using by IDE only. It is NOT the R class actually packed into APK */
-public final class R {
-}
